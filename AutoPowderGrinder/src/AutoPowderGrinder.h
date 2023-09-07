0bb08ca7e677e0ead42770f89c5dba2e09d81fe3
cristache carol <cristachecarol13@gmail.com>
2023-09-07 12:43:29 +0300

    updated nonsolids list for pathfinding

--- AutoPowderGrinder/src/AutoPowderGrinder.h
+++ AutoPowderGrinder/src/AutoPowderGrinder.h
@@ -255,7 +255,15 @@ namespace apg
 		inline static std::set<int>
 			blocksToBreak = { 1, 14, 15, 16, 21, 56, 73, 74, 129 },
 			blocksToOpen = { 54, 146 },
-			nonSolid = { 0, 8, 9, 10, 11 };
+			nonSolid = {
+				0, 6, 8, 9, 10, 11, 27, 28, 
+				30, 31, 32, 37, 38, 39, 40, 
+				50, 51, 55, 59, 63, 65, 66, 
+				68, 69, 70, 72, 75, 76, 77, 
+				83, 90, 104, 105, 106, 115, 
+				131, 132, 141, 142, 143, 147, 
+				148, 157, 175, 176, 177, 207 
+			};
 
 		inline static std::shared_ptr<AutoPowderGrinder::Minecraft> minecraft{ nullptr };
 
